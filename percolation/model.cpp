ba6510a61997e50eca5a0cefe1037da3ab6396c3
xethrus <xethrus@gmail.com>
2023-01-16 21:16:21 -0800

    indeed

--- percolation/model.cpp
+++ percolation/model.cpp
@@ -2,26 +2,44 @@
 #include "node.h"
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 class Percolation {
 public:
   std::vector<std::vector<Node*>> nodeArray;
   QuickFindUF* checker;
+  std::vector<Node*> topNodes;
+  std::vector<Node*> bottomNodes;
+  int gridSize = 0;
   Percolation(int n) {
+    std::vector<std::vector<Node*>> newArray(n, std::vector<Node*>(n));
+    nodeArray = newArray;
     checker = new QuickFindUF(n);
-    int fullSize = n * n;
-    nodeArray.resize(n);
+
+    gridSize = n;
     for(int i = 0; i < n; i++) {
-      nodeArray[i].resize(n);
-      for(int j = 0; j < n; i++) {
+      for(int j = 0; j < n; j++) {
         Node* newNode = new Node();
         nodeArray[i][j] = newNode;
-        newNode->xyCord = std::make_pair(i,j);
+        newNode->xyCord = std::make_pair(i,j);  
+        if(j == 0) {
+          topNodes.push_back(newNode);
+          std::cout << newNode->xyCord.first << " and " <<
+            newNode->xyCord.second << std::endl;
+        }
+        if(j == n-1) {
+          bottomNodes.push_back(newNode);
+          std::cout << newNode->xyCord.first << " and " <<
+            newNode->xyCord.second << std::endl;
+        }
       }
     }
   }
 
   void open(int, int);
+  bool isOpen(int, int);
+  int openSites();
+  bool percolates();
 };
 
 void Percolation::open(int x, int y) {
@@ -50,6 +68,43 @@ void Percolation::open(int x, int y) {
   }
 }
 
+bool Percolation::isOpen(int x, int y) {
+  if(this->nodeArray[x][y]->isOpen) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+int Percolation::openSites() {
+  int openCount = 0;
+  for(int y = 0; y < this->gridSize; y++) {
+    for(int x = 0; x <this->gridSize; x++) {
+      if(!this->nodeArray[x][y]->isOpen) {
+        openCount++;
+      }
+    }
+  }
+  return openCount;
+}
+
+bool Percolation::percolates() {
+  int sizeOfCurrent = this->checker->arraySize;
+  for(int i = 0; i < this->topNodes.size(); i++) {
+    for(int j = 0; j < this->bottomNodes.size(); j++) {
+      if(this->checker->id[this->topNodes[i]->number] ==
+         this->checker->id[this->bottomNodes[j]->number]) {
+        std::cout << "yay" << std::endl;
+        return true;
+      }
+    }
+  }
+  std::cout << "nay" << std::endl;
+  return false;
+}
+
 int main() {
+  Percolation test(5);
+  
   return 0;
 }
--- percolation/node.h
+++ percolation/node.h
@@ -1,9 +1,14 @@
+#include <utility>
+
 class Node {
 public:
+  std::pair<int, int> xyCord;
+  int number;
   bool isOpen;
   //todo maybe add stuff lol
   Node() {
     isOpen = false;
+    number = 0;
   }
   ~Node() {
     delete this;
