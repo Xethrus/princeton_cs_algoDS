2e7b83d6e97a1501edbc0d124f91dc41cc544f72
xethrus <xethrus@gmail.com>
2023-01-19 22:03:51 -0800

    yes indeed

---
+++ bagsQueuesStacks/dikstraArithmiticEval.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <vector>
+#include <cmath>
+
+template <typename T>
+class Node {
+public:
+  T data;
+  Node* next;
+  
+  Node(T data) {
+    this->data = data;
+    next = nullptr;
+  }
+};
+
+template <typename T>
+class Stack {
+public:
+  Node<T>* top;
+  int totalSize;
+  Stack() {
+    top = nullptr;
+  }
+  void push(T);
+  T pop();
+};
+
+
+template <typename T>
+void Stack<T>::push(T element) {
+Node<T>* newNode = new Node<T>(element);
+  if(this->top == nullptr) {
+    this->top = newNode;
+    return;
+  } else {
+    Node<T>* placeHolder = this->top;
+    this->top = newNode;
+    newNode->next = placeHolder;
+    return;
+  }
+}
+
+template <typename T>
+T Stack<T>::pop() {
+  Node<T>* placeHolder = this->top;
+  this->top = this->top->next;
+  return placeHolder;
+}
+
+class ArithmaticDualStack {
+public:
+  Stack<int> operandStack;
+  Stack<char> operatorStack;
+
+  double solveInput();
+};
+
+double ArithmaticDualStack::solveInput() {
+  std::cout << "enter you arithmatic: ";
+  std::string equation;
+  std::getline(std::cin, equation);
+
+  for(int i = 0; i < equation.size() - 1; i++) {
+    std::vector<int> number;
+    number.clear();
+    while(std::isdigit(equation[i])) {
+      number.push_back(equation[i]);
+      i++;
+    }
+    if(!number.empty()){ 
+      int numberPlaces = number.size();
+      int theNumber = 0;
+      for(int i = 0; i < numberPlaces; i++) {
+        int multi = std::pow(10,i);
+        theNumber += number[numberPlaces-i] * multi;
+      }
+      this->operandStack.push(theNumber);
+    }
+    switch(equation[i]) {
+      case "+": this->operatorStack->push('+');
+      case "-": this->operatorStack->push('-');
+      case "*": this->operatorStack->push('*');
+      case "/": this->operatorStack->push('/');
+
+    }
+    if(equation[i] == ")") {
+      switch(this->operandStack->pop()) {
+        case '+': this->operandStack->push(
+          (this->operandStack->pop() + this->operandStack->
+            pop()));
+        case '-': this->operandStack->push(
+          (this->operandStack->pop() - this->operandStack->
+            pop()));
+        case '*': this->operandStack->push(
+          (this->operandStack->pop() * this->operandStack->
+            pop()));
+        case '/': this->operandStack->push(
+          (this->operandStack->pop() / this->operandStack->
+            pop()));
+      }
+    }
+  }
+  return this->operandStack->pop();
+}
+
+};
+
+
+int main() {
+  return 0;
+}
---
+++ bagsQueuesStacks/stack_example.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+
+class Node {
+public:
+  std::string info;
+  Node* next;
+  Node() {
+  }
+  Node(std::string data) {
+    info = data;
+    next = nullptr;
+  }
+};
+
+class Stack {
+public:
+  Node* top;
+  int totalSize = 0;
+  Stack() {
+    top = nullptr;
+    totalSize = 0;
+  }
+  void push(std::string);
+  std::string pop();
+  bool isEmpty();
+};
+
+void Stack::push(std::string info) {
+  Node* newNode = new Node(info);
+  this->totalSize++;
+  if(top == nullptr) {
+    this->top = newNode;
+    return;
+  }
+  Node* next = this->top;
+  this->top = newNode;
+  newNode->next = next;
+}
+
+std::string Stack::pop() {
+  Node* returnVal = this->top;
+  this->top = this->top->next;
+  return returnVal->info;
+}
+
+bool Stack::isEmpty() {
+  if(top == nullptr) {
+    return true;
+  }
+  return false;
+}
+
+int main() {
+  Stack test;
+  test.push("a");
+  test.push("b");
+  test.push("c");
+  test.push("d");
+  test.push("e");
+  test.push("f");
+  test.push("g");
+  std::cout << test.pop() << std::endl;
+  std::cout << test.pop() << std::endl;
+  std::cout << test.pop() << std::endl;
+  std::cout << test.pop() << std::endl;
+  std::cout << test.pop() << std::endl;
+  std::cout << test.pop() << std::endl;
+  std::cout << test.pop() << std::endl;
+  return 0;
+}
