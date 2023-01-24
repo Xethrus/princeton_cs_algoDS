b7dca5d1fc635305e07abc95c7e953528110ed5e
xethrus <xethrus@gmail.com>
2023-01-23 20:48:20 -0800

    broken

--- sortingAlgos/mergeSort.cpp
+++ sortingAlgos/mergeSort.cpp
@@ -1,37 +1,33 @@
 #include <iostream>
 #include <vector>
 
-void mergeSort(std::vector<int> v) {
-  std::vector<int> halveOne;
-  std::vector<int> halveTwo;
-  int totalSize = v.size();
-  int midPoint = totalSize/2;
-  for(int i = 0; i < totalSize; i++) {
-    if(i < midPoint) {
-      halveOne.push_back(v[i]);
+std::vector<int> mergeSort(std::vector<int> v) {
+  int mid, totalSize;
+  totalSize = v.size();
+  mid = totalSize/2;
+  int start1, end1, start2, end2;
+  start1 = 0;
+  end1 = mid-1;
+  start2 = mid;
+  end2 = totalSize-1;
+  std::vector<int> aux;
+  for(int k = start1; k <= end2; k++) {
+    aux[k] = v[k];
+  }
+  int i, j;
+  i = start1;
+  j = start2;
+  for(int k = start1; k <= end2; k++) {
+    if(start1 > start2) { v[k] = aux[j++]; }
+    else if(j > end2) { v[k] = aux[i++]; }
+    else if(aux[j] < aux[i]) {
+      v[k] = aux[j++];
     } else {
-      halveTwo.push_back(v[i]);
+      v[k] = aux[i++];
     }
   }
-  std::vector<int> halveOneSorted = selectionSort(halveOne);
-  std::vector<int> halveTwoSorted = selectionSort(halveTwo);
+  return aux;
 }
-
-std::vector selectionSort(std::vector<int> v) {
-  int min;
-  for(int i = 0; i < v.size(); i++) {
-    min = i;
-    for(int j = i; j < v.size(); j++) {
-      if(v[j] < v[min]) {
-        min = j;
-      }
-    }
-    swap = v[min];
-    v[min] = v[i];
-    v[i] = swap;
-  }
-}
-
 
 int main() {
   return 0;
